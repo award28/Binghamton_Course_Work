13467c815f791d3e0142be4cb95a1c7bb132ed56
award28 <ward.austin28@gmail.com>
2016-02-19 23:13:01 -0500

    Finished HW3.txt

--- homework/test.c
+++ homework/test.c
@@ -2,11 +2,11 @@
 #include <math.h>
 
 int main(int argc, char **argv) {
-   float exp = -1.0;
+   float exp = 140.0;
    float current = powf(2.0, exp);
    for(int i = 0; i < 20; i++) { 
        current = powf(2.0, exp);
-       printf("%f\n",current);
+       printf("%g -- %f\n",current,exp);
         exp -= 1.0;
    }
 }
