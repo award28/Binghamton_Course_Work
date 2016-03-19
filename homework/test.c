be2b0e5baaeaf5caf799fc95818730bb8f500fb7
award28 <ward.austin28@gmail.com>
2016-03-18 22:51:38 -0400

    Finished hw6

--- homework/test.c
+++ homework/test.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
+	enum strikers { Giroux, Bony, Rooney, Hazard, Aguero, Costa, Kane } player;
+	enum teams { Arsenal, ManCity, ManU, Chelsea, Tottenham } team;
+	
 int main(int argc, char **argv) {
-	int matrix[2][3];
-	int *mptr = &matrix[0][0];
-	int row; int col;
-	for(row=0; row<2; row++) {
-		for(col=0; col<3; col++) {
-			matrix[row][col] = (row*10) + col;
-			printf("%d\n", matrix[row][col]);
-		}
-	}
+	
+    player = Giroux;
 
-	printf("The fifth element of matrix is %d\n",*(mptr+5));
+	if (player==Giroux) { team=Arsenal; printf("Working"); }
+	else if (player==Bony) team=ManCity;
+	else if (player==Rooney) team=ManU;
+        
+
 }
