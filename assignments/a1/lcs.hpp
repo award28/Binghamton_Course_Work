065d9c303948d5ab1331e04c5e44f97e67707bb5
award28 <ward.austin28@gmail.com>
2017-03-15 11:14:36 -0400

    Finished task 1/prepped for task 2 & 3

---
+++ assignments/a1/lcs.hpp
@@ -0,0 +1,12 @@
+#ifndef LCS_HPP
+#define LCS_HPP
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class LenLCS {
+    public:
+        LenLCS() {}
+        void execute(const std::string &x, const std::string &y, const std::string &outfile);
+};
+#endif
