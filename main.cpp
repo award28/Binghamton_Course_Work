7beed16e0a32d1784c5d38811b764bc9f345dd81
Sean Gallagher <sgallag3@cs.binghamton.edu>
2017-05-04 19:26:24 -0400

    blah

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <pthread.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+typedef struct diskOpArg {
+	string fileName;
+	//Controller &controller;
+} diskOpArg;
+	
+
+int main(int argc, char* argv[]){
+	int numThreads, i;
+	char *diskName;
+	char *file[4];
+	
+	if(argc < 3 || argc > 6){
+		cout << "Usage ./" << argv[0] << "<disk file name> <thread1 file name> ..." << endl;
+		exit(1);
+	} 
+
+	diskName = argv[1];
+
+	numThreads = argc - 2;
+	for(i = 2; i < argc; i++){
+		file[i - 2] = argv[i];
+	}
+
+	pthread_t threads[numThreads];
+	for(i = 0; i < numThreads; i++){
+		diskOpArg temp;
+		temp.fileName = file[i];
+		pthread_create(&threads[i], NULL, disk_op, (void *)temp);
+		cout << file[i] << endl;
+	}
+}
