df477faaef119ef67027b70862483c5e2680fc40
Ethan Schoen <e.schoen@yahoo.com>
2017-05-04 18:42:35 -0400

    disk_op.cpp added for disk op threads. Proabably mostely done

---
+++ disk_op.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <ssfs.hpp>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+
+using std::string;
+
+disk_op::disk_op(string f_name, Controller &controller) {
+    std::ifstream in;
+    in.open(f_name);
+    if (in.fail()) {
+        std::cerr << "Error: " << strerror(errno);
+    }
+    op curr_op;
+    string line;
+    while (in >> line) {
+        curr_op.command = line;
+        curr_op.pid = getpid();
+        controller.execute(curr_op);
+    }
+}
--- ssfs.hpp
+++ ssfs.hpp
@@ -3,32 +3,33 @@
 #include <iostream>
 #include <string>
 #include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 struct SuperBlock {  
     int dataBlocks;
     int numInodes;
 };
 
+struct op {
+    std::string command;
+    pid_t pid;
+};
+
 class Controller {
     public:
         char* read();
         bool write(char data[]);
+        void execute(op command);
     private:
         bool bitmap[256] = {false};
         bool inodeTable[256] = {false};
+        std::queue<op> exe_queue;
 };
-
-struct op {
-    std::string command;
-    pthread_t pid;
-};
-//std::queue<op> exe_queue;
 
 class disk_op {
     public:
         disk_op(string f_name);
-    private:
-        run(
-}
+};
 
 #endif
